210415542faccd2f52162aca523d5a43ca401ed5
Relativiteit <alejocain@gmail.com>
2022-01-14 09:44:01 +0100

    query

--- Chapter21/DemoClass.java
+++ Chapter21/DemoClass.java
@@ -22,19 +22,25 @@ public class DemoClass {
         String url = "jdbc:mysql://localhost:3306/alien?allowPublicKeyRetrieval=true&useSSL=false"; 
         String uname = "admin";
         String pass = "password";
-        String query = "select username from student where userid=3";
+        String query = "insert into student values (4, 'Mohini')";
 
     Class.forName("com.mysql.cj.jdbc.Driver");
     Connection con = DriverManager.getConnection(url, uname, pass);
     Statement st = con.createStatement();
     st.executeQuery(query);
-    ResultSet rs = st.executeQuery(query);
+    ResultSet rs = st.executeQuery(query)); // DDL, DML ,DQL
+        // DDL Data definition language change the structure of the database
+        // DML Data manipulation language changing data inserting values updating existing rows
+        // DQL Data query language fetching data query
 
-    rs.next();
+    String userData = "";
 
-    String name = rs.getString("userName");
+    while(rs.next()) {
 
-    System.out.println(name);
+        userData = rs.getInt(1) + " : " + rs.getString(2);
+        System.out.println(userData);
+    }
+
 
     st.close();
     con.close();
