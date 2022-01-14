b19c36469f9f00d39f1c8601f2426402e340b137
Relativiteit <alejocain@gmail.com>
2022-01-14 11:58:12 +0100

    dao adding student

--- Chapter21/JdbcDaoDemo.java
+++ Chapter21/JdbcDaoDemo.java
@@ -7,22 +7,34 @@ public class JdbcDaoDemo {
 
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        Student s1 = dao.getStudent(13);
-        System.out.println(s1.sname);
+        Student s2 = new Student();
+        s2.rollno = 15;
+        s2.sname = "Mariachan";
+        dao.connect();
+        dao.addStudent(s2);
     }
 }
 
 
     class StudentDAO {
+        Connection con = null;
+        public void connect()
+        {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost/studyq","admin","password");
 
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
         public Student getStudent(int rollno){
 
             try {
                 Student s = new Student();
                 s.rollno = rollno;
                 String query = "select sname from student where rollno=" + rollno;
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost/studyq","admin","password");
+
                 Statement st = con.createStatement();
                 ResultSet rs = st.executeQuery(query);
 
@@ -36,7 +48,25 @@ public class JdbcDaoDemo {
             return null ;
 
         }
+
+        public void addStudent(Student s)
+        {
+            String query = "insert into student values (?,?)";
+            PreparedStatement pst = null;
+            try {
+                pst = con. prepareStatement(query);
+                pst.setInt(1, s.rollno );
+                pst.setString(2, s.sname );
+                pst.executeUpdate();
+
+
+
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
     }
+
 
     class Student
     {
